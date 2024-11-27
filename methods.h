6d1d8a0f3be443b3b9e1a6e2e803da7f1d652501
jreiff627 <146307727+jreiff627@users.noreply.github.com>
2024-11-26 19:44:46 -0600

    Update methods.h
    Signed-off-by: jreiff627 <146307727+jreiff627@users.noreply.github.com>

--- methods.h
+++ methods.h
@@ -9,4 +9,12 @@ int fullScan(int startDeg, int endDeg);
 void printIR(oi_t *sensor);
 void sendToPutty(char string[], int length);
 
+void objectAvoid(oi_t *sensor);
+void secondObject(oi_t *sensor);
+void manualDriver(oi_t* sensor_data);
+void find_bathroom(oi_t* sensor_data);
+void find_kitchen(oi_t* sensor_data);
+void find_livingRoom(oi_t* sensor_data);
+void find_exit(oi_t* sensor_data);
+
 #endif
