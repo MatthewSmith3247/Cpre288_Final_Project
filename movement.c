2064039769b6a7089d7290fcf0e176ea5b468115
jreiff627 <146307727+jreiff627@users.noreply.github.com>
2024-11-28 00:03:49 -0600

    Update movement.c
    Signed-off-by: jreiff627 <146307727+jreiff627@users.noreply.github.com>

--- movement.c
+++ movement.c
@@ -12,7 +12,7 @@ while (sum < degrees)
     oi_update(sensor);
     sum += sensor->angle;
 }
-
+cybot_send_string("counterclockwise\n");
 oi_setWheels(0, 0); //stop
 }
 
@@ -26,19 +26,21 @@ while (sum < degrees)
     oi_update(sensor);
     sum -= sensor->angle;
 }
-
+cybot_send_string("clockwise\n");
 oi_setWheels(0, 0); //stop
 }
 
 void moveForward(oi_t *sensor, int centimeters) {
     double sum = 0;
     oi_setWheels(50, 50);
+    char sendData[50];
     while (sum < centimeters)
     {
         oi_update(sensor);
         sum += sensor->distance;
     }
-
+    sprintf(sendData, "moved\t%d\n", centimeters);
+    cybot_send_string(sendData);
     oi_setWheels(0, 0); // stop
 }
 
